68b91bce8687c089a71fab26afa3bcf9fd8a46e3
Leonardo Spuldar <spdleonardo@hotmail.com>
2023-02-28 21:25:52 -0300

    create space

---
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -0,0 +1,43 @@
+package com.manning.apisecurityinaction;
+ 
+import com.manning.apisecurityinaction.controller.*;
+import org.dalesbred.Database;
+import org.h2.jdbcx.JdbcConnectionPool;
+import org.json.*;
+ 
+import java.nio.file.*;
+ 
+import static spark.Spark.*;
+ 
+
+ // mvn clean compile exec:java
+public class Main {
+ 
+  public static void main(String... args) throws Exception {
+    var datasource = JdbcConnectionPool.create(
+        "jdbc:h2:mem:natter", "natter", "password");
+    var database = Database.forDataSource(datasource);
+    createTables(database);
+ 
+    var spaceController = new SpaceController(database);     
+    post("/spaces", spaceController::createSpace);     
+ 
+    after((request, response) -> {         
+      response.type("application/json");   
+    });
+ 
+    internalServerError(new JSONObject()
+      .put("error", "internal server error").toString());
+    notFound(new JSONObject()
+      .put("error", "not found").toString());
+  }
+
+  
+  private static void createTables(Database database) 
+      throws Exception {
+    var path = Paths.get(                                    
+        Main.class.getResource("/schema.sql").toURI());      
+    database.update(Files.readString(path));                 
+  }
+}
+
