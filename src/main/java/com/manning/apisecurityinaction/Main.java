7c903c849b64345df28f8c78061ad2cd5bf7c955
Leonardo Spuldar <spdleonardo@hotmail.com>
2023-03-04 22:14:44 -0300

    ch 2

--- src/main/java/com/manning/apisecurityinaction/Main.java
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -1,5 +1,6 @@
 package com.manning.apisecurityinaction;
- 
+
+import com.google.common.util.concurrent.*;
 import com.manning.apisecurityinaction.controller.*;
 import org.dalesbred.Database;
 import org.h2.jdbcx.JdbcConnectionPool;
@@ -19,15 +20,45 @@ public class Main {
     datasource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter_api_user", "password");    
     database = Database.forDataSource(datasource);
 
-    var spaceController = new SpaceController(database);     
-    post("/spaces", spaceController::createSpace);     
+    var rateLimiter = RateLimiter.create(2.0d);       
+    var userController = new UserController(database);
+    var spaceController = new SpaceController(database); 
+
+    // ------------- BEFORE --------------
+    before(userController::authenticate);
+    before((request, response) -> {
+      if (!rateLimiter.tryAcquire()) {                
+        response.header("Retry-After", "2");          
+        halt(429);                                    
+      }
+    });
+    before(((request, response) -> {  
+      if (request.requestMethod().equals("POST") &&             
+          !"application/json".equals(request.contentType())) {  
+        halt(415, new JSONObject().put(                         
+            "error", "Only application/json supported"
+        ).toString());
+      }
+    }));
+
+    // ------------- ROUTES --------------
+    post("/users", userController::registerUser);
+    post("/spaces", spaceController::createSpace); 
  
+
+     // ------------- AFTER --------------
     after((request, response) -> {         
       response.type("application/json");   
     });
     afterAfter((request, response) -> {
-      response.header("Server", "")
-      response.header("X-XSS-Protection", "0")
+      response.type("application/json;charset=utf-8");
+      response.header("X-Content-Type-Options", "nosniff");
+      response.header("X-Frame-Options", "DENY");
+      response.header("X-XSS-Protection", "0");
+      response.header("Cache-Control", "no-store");
+      response.header("Content-Security-Policy",
+        "default-src 'none'; frame-ancestors 'none'; sandbox");
+      response.header("Server", "");
     });
  
     internalServerError(new JSONObject()
@@ -39,7 +70,6 @@ public class Main {
     exception(JSONException.class, Main::badRequest);
     exception(EmptyResultException.class, (e, request, response) -> response.status(404));
   }
-
   
   private static void createTables(Database database) 
       throws Exception {
@@ -50,7 +80,6 @@ public class Main {
 
   private static void badRequest(Exception ex, Request request, Response response) {
     response.status(400);
-    response.body("{\"error\":\"" + ex.getMessage() + "\"}");
+    response.body(new JSONObject().put("error", ex.getMessage()).toString());
   }
 }
-
--- src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
+++ src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
@@ -15,15 +15,20 @@ public class SpaceController {
       throw new IllegalArgumentException("space name too long");
     }
     var owner = json.getString("owner");
+    var subject = request.attribute("subject");
+    if(!owner.equals(subject)) {
+        throw new IllegalArgumentException(
+          "owner must match authenticated user"
+        );
+    }
+
     if (!owner.matches("[a-zA-Z][a-zA-Z0-9]{1,29}")) {
-      throw new IllegalArgumentException("invalid username: " + owner);
+      throw new IllegalArgumentException("invalid username");
     }
 
     return database.withTransaction(tx -> {                
       var spaceId = database.findUniqueLong(               
           "SELECT NEXT VALUE FOR space_id_seq;");          
-      // WARNING: this next line of code contains a
-      // security vulnerability!
       database.updateUnique(
           "INSERT INTO spaces(space_id, name, owner) " +
               "VALUES(?, ? , ?);", spaceId, spaceName, owner);
@@ -33,5 +38,35 @@ public class SpaceController {
           .put("name", spaceName)
           .put("uri", "/spaces/" + spaceId);
     });  
+  }
+
+
+    // Additional REST API endpoints not covered in the book:
+  public JSONObject postMessage(Request request, Response response) {
+    var spaceId = Long.parseLong(request.params(":spaceId"));
+    var json = new JSONObject(request.body());
+    var user = json.getString("author");
+    if (!user.matches("[a-zA-Z][a-zA-Z0-9]{0,29}")) {
+      throw new IllegalArgumentException("invalid username");
+    }
+    var message = json.getString("message");
+    if (message.length() > 1024) {
+      throw new IllegalArgumentException("message is too long");
+    }
+
+    return database.withTransaction(tx -> {
+      var msgId = database.findUniqueLong(
+          "SELECT NEXT VALUE FOR msg_id_seq;");
+      database.updateUnique(
+          "INSERT INTO messages(space_id, msg_id, msg_time," +
+              "author, msg_text) " +
+              "VALUES(?, ?, current_timestamp, ?, ?)",
+          spaceId, msgId, user, message);
+
+      response.status(201);
+      var uri = "/spaces/" + spaceId + "/messages/" + msgId;
+      response.header("Location", uri);
+      return new JSONObject().put("uri", uri);
+    });
   }
 }
